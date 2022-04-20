07dce6b8879d80709c18f41ca2db90365e0fd5f8
iseunghan <tmdgks301@naver.com>
2022-04-20 21:46:27 +0900

    [Mockito] init

---
+++ mockito-framework/src/main/java/me/iseunghan/mockitoframework/MockitoFrameworkApplication.java
@@ -0,0 +1,13 @@
+package me.iseunghan.mockitoframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoFrameworkApplication.class, args);
+    }
+
+}
---
+++ mockito-framework/src/test/java/me/iseunghan/mockitoframework/MockitoFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iseunghan.mockitoframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
